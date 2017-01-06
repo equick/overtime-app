575926cdddbb3556e58c91c5c9e6d9b45e8cd262
equick <edwardquick@hotmail.com>
2017-01-06 21:01:55 +0000

    Restricted list of posts to show only those created by a user and hiding others

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -26,6 +26,16 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Rationale|content/)
     end
+
+    it 'has a scope so that only post creators can their posts' do
+      post1 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+      other_user = User.create(first_name: "Other", last_name: "User", email: "otheruser@test.com", password: "asdasd", password_confirmation: "asdasd")
+      post_from_other_user =  Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+
+      visit posts_path
+      expect(page).to_not have_content(/This post shouldn't be seen/)
+    end
   end
 
   describe 'new' do
@@ -40,6 +50,8 @@ describe 'navigate' do
   describe 'delete' do
     it 'can be deleted' do
       @post = FactoryGirl.create(:post)
+      # TODO
+      @post.update(user_id: @user.id)
       visit posts_path
 
       click_link("delete_post_#{@post.id}_from_index")
