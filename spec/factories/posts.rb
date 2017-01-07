e577a9a8d80de2947b14ebb416015890bb876869
equick <edwardquick@hotmail.com>
2017-01-07 11:23:08 +0000

    Added overtime hours requested

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -4,17 +4,19 @@ FactoryGirl.define do
     date Date.today
     rationale "Some other Rationale"
     user
+    overtime_request 3.5
   end
 
   factory :second_post, class: 'Post' do
     date Date.yesterday
     rationale "Some more content"
     user
+    overtime_request 0.5
   end
 
-  factory :post_from_other_user, class: 'Post' do
-    date Date.yesterday
-    rationale "Some more content"
-    non_authorized_user
-  end
+  # factory :post_from_other_user, class: 'Post' do
+  #   date Date.yesterday
+  #   rationale "Some more content"
+  #   non_authorized_user
+  # end
 end
