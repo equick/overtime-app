a11a919ae0f6cab91d5317690ba3034967d1359f
equick <edwardquick@hotmail.com>
2017-01-07 16:28:44 +0000

    Added phone attribute to User and made it required

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -31,7 +31,12 @@ describe 'navigate' do
     end
 
     it 'has a scope so that only post creators can their posts' do
-      other_user = User.create(first_name: "Other", last_name: "User", email: "otheruser@test.com", password: "asdasd", password_confirmation: "asdasd")
+      other_user = User.create(first_name: "Other",
+                               last_name: "User",
+                               email: "otheruser@test.com",
+                               password: "asdasd",
+                               password_confirmation: "asdasd",
+                               phone: "234234")
       post_from_other_user =  Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id, overtime_request: 3.5)
 
       visit posts_path
