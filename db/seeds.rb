e54f5b22c68241d0dfd9e1d1bfe19b423c7b81f8
equick <edwardquick@hotmail.com>
2017-01-09 20:13:37 +0000

    Corrected issue in seeds file creating AuditLog entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,35 @@
-@user = User.create(email: "test@test.com",
+@user = User.create!(email: "test@test.com",
                     password: "asdfasdf",
                     password_confirmation: "asdfasdf",
                     first_name: "Jon",
                     last_name: "Snow",
-                    phone: "447538092927")
+                    phone: "0123456789")
 
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com",
+AdminUser.create!(email: "admin@test.com",
                  password: "asdfasdf",
                  password_confirmation: "asdfasdf",
                  first_name: "Admin",
                  last_name: "Name",
-                 phone: "447538092927")
+                 phone: "0123456789")
 
 puts "1 Admin User created"
+
+# AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+# AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+# AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+# 
+# puts "3 Audit Logs have been created"
+
+100.times do |post|
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "100 Audit logs have been created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
-
-100.times do |audit_log|
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-end
-
-puts "100 Audit Logs have been created"
