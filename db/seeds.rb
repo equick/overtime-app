a7718bb3b0dcea1e04a10b2ded0b33ba462a4b53
equick <edwardquick@hotmail.com>
2017-01-14 23:09:22 +0000

    Styled the employee homepage pending audit approvals

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,17 +16,17 @@ AdminUser.create!(email: "admin@test.com",
 
 puts "1 Admin User created"
 
-# AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-# AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-# AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-# 
-# puts "3 Audit Logs have been created"
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
-100.times do |post|
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-end
+puts "3 Audit Logs have been created"
 
-puts "100 Audit logs have been created"
+# 100.times do |post|
+#   AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+# end
+
+# puts "100 Audit logs have been created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
