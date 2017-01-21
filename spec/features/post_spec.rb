95f7be0e154b4c909cd485b87167d342ef03686b
equick <edwardquick@hotmail.com>
2017-01-21 16:28:12 +0000

    Updated nav to remove components not needed for admins

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -46,6 +46,13 @@ describe 'navigate' do
 
   describe 'new' do
     it 'has a link from the homepage' do
+      employee = Employee.create(first_name: "Employee",
+                             last_name: "Authorized",
+                             email: "employee@example.com",
+                             password: "asdasd",
+                             password_confirmation: "asdasd",
+                             phone: "1234567890")
+      login_as(employee, :scope => :user)
       visit root_path
 
       click_link("new_post_from_nav")
